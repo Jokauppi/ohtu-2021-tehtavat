77ce2f643986af62c74c2512a3351b6ec7eefda3
Joona Kauppila <joona.kauppila@helsinki.fi>
2021-11-15 23:34:29 +0200

    Viikko 2 tehtävä 11

--- viikko2/verkkokauppa-1/src/index.py
+++ viikko2/verkkokauppa-1/src/index.py
@@ -5,11 +5,13 @@ from pankki import Pankki
 from viitegeneraattori import Viitegeneraattori
 
 def main():
-    kauppa = Kauppa(
-        Varasto.get_instance(),
-        Pankki.get_instance(),
-        Viitegeneraattori.get_instance()
-    )
+
+    kirjanpito = Kirjanpito()
+    varasto = Varasto(kirjanpito)
+    pankki = Pankki(kirjanpito)
+    viitegeneraattori = Viitegeneraattori()
+
+    kauppa = Kauppa(varasto, pankki, viitegeneraattori)
 
     # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
     kauppa.aloita_asiointi()
@@ -28,7 +30,7 @@ def main():
     kauppa.tilimaksu("Arto Vihavainen", "3425-1652")
 
     # kirjanpito
-    for tapahtuma in Kirjanpito.get_instance().tapahtumat:
+    for tapahtuma in kirjanpito.tapahtumat:
         print(tapahtuma)
 
 
