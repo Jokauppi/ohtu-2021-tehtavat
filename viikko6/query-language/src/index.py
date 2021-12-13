0f5d056c203bec296a1e3f1e2b03a6e2f0ce7d71
Joona Kauppila <joona.kauppila@helsinki.fi>
2021-12-13 23:03:52 +0200

    Viikko 6 tehtävä 2

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,45 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import *
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+        HasAtLeast(5, "goals"),
+        HasAtLeast(5, "assists"),
+        PlaysIn("PHI")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+    print("============")
+
+    matcher = Not(All())
+
+    for player in stats.matches(matcher):
+        print(player)
+    print("============")
+
+    matcher = And(
+        Not(HasAtLeast(1, "goals")),
+        PlaysIn("NYR")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+    print("============")
+
+    matcher = And(
+        HasFewerThan(5, "goals"),
+        PlaysIn("PHI")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+    print("============")
+
+if __name__ == "__main__":
+    main()
